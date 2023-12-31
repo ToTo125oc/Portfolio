d0440134035e72bfc0b2977a403a011a963c2ace
Thomas <thomas.royer125@tutamail.com>
2023-12-31 16:04:49 +0100

    ajout de la migrations pour les projets completes

--- database/migrations/2023_12_28_194659_create_projets_tables.php
+++ database/migrations/2023_12_28_194659_create_projets_tables.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projets', function (Blueprint $table) {
             $table->id();
+            $table->string("titre");
+            $table->string("imageProjet");
+            $table->string("contenu");
             $table->timestamps();
         });
     }
