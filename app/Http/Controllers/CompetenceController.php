3d908ae3b66b7d9d6d4133ab7f7b18fd275de0f3
Thomas <thomas.royer125@tutamail.com>
2023-12-31 17:05:16 +0100

    ajout du controller pour les competences

---
+++ app/Http/Controllers/CompetenceController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Competence;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class CompetenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $competences = Competence::all();
+        return view("competences.index",['competences'=>$competences]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'nom'=>'required',
+            'description'=>'required',
+            'imageCompetence'=>'required',
+        ]);
+        $competence = new Competence();
+        $competence->titre = $request->titre;
+        $competence->description = $request->description;
+        if($request->hasFile('imageCompetence') && $request->file('imageCompetence')->isValid()){
+            $file = $request->file('imageCompetence');
+            $nom = sprintf('%s_%d.%s','imageCompetence', time(), $file->extension());
+            $file->storeAs('competences',$nom);
+            $competence->imageCompetence = "competences/".$nom;
+        }
+        $competence->save();
+        return redirect()->route('competences.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $competence = Competence::find($id);
+        return view("competences.show",['competence'=>$competence]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $this->validate($request,[
+            'nom'=>'required',
+            'description'=>'required',
+            'imageCompetence'=>'required',
+        ]);
+        $competence = Competence::find($id);
+        $competence->titre = $request->titre;
+        $competence->description = $request->description;
+        if($request->hasFile('imageCompetence') && $request->file('imageCompetence')->isValid()){
+            if (isset($competence->imageProjet)) {
+                Storage::delete($competence->imageProjet);
+            }
+            $file = $request->file('imageCompetence');
+            $nom = sprintf('%s_%d.%s','imageCompetence', time(), $file->extension());
+            $file->storeAs('competences',$nom);
+            $competence->imageCompetence = "competences/".$nom;
+        }
+        $competence->save();
+        return redirect()->route('competences.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, string $id)
+    {
+        $competence = Competence::find($id);
+        if($request->delete == "Supprimer"){
+            if (isset($competence->imageProjet)) {
+                Storage::delete($competence->imageProjet);
+            }
+            $competence->delete();
+        }
+        return redirect()->route('projets.index');
+    }
+}
