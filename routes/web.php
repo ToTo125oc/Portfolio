3ccd992d87f5dadda4a735587cb7261143e920f7
Thomas <thomas.royer125@tutamail.com>
2023-12-31 17:25:43 +0100

    ✨  Ajout des vues vides

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CompetenceController;
+use App\Http\Controllers\ProjetsController;
+use App\Http\Controllers\ReseauController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/projets', ProjetsController::class);
+Route::resource('/reseaux', ReseauController::class);
+Route::resource('/competences', CompetenceController::class);
