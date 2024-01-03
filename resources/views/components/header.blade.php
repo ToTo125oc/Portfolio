b99c4c018794453dfca283dfce25d7cb9a120a48
Thomas <thomas.royer125@tutamail.com>
2024-01-03 21:52:30 +0100

    ajout bouton logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -21,6 +21,14 @@
         <li>
           <a href="{{Route("contact.index")}}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
+        @auth
+        <li>  
+          <form action="{{ route('logout') }}" method="POST" class="block py-2 px-3 text-white bg-red-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-red-500">
+            @csrf
+            <input type="submit" value="Déconnexion"/>
+          </form>
+        </li>
+        @endauth
       </ul>
     </div>
   </div>
